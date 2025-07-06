005b3d6fe6941f9780c09ca2a3a4a4c9f6b7fffd
CJ <caylemh@gmail.com>
2025-07-06 21:30:06 +0200

    Add Book constructor

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
-const myLibrary = [];
+const myLibrary = [
 
-function Book() {
-  // the constructor...
+];
+
+function Book(title, author, pages, read){
+    if(!new.target){
+        throw Error("You must use the 'new' operator before calling this constructor.");
+    }
+    this.id = crypto.randomUUID()   // Use to create a unique ID for each book
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
 }
+  
+function addBookToPage() {
+
+}
+
+/* Pseudocode
+
+*/
