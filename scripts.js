269282606057cdf8c76ff5360fab2c97fa3f1d65
CJ <caylemh@gmail.com>
2025-07-07 21:09:56 +0200

    Add functiionality to add book cards to page

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 const myLibrary = [
-
+    {id: 123456,
+      title: "The Hobbit",
+      author: "JK Rowling",
+      pages: 310,
+      read: "Yes",
+    },
+    {id: 112345,
+      title: "The Alchemist",
+      author: "Paulo Coelho",
+      pages: 210,
+      read: "No",
+    },
 ];
 
 function Book(title, author, pages, read){
@@ -17,9 +28,27 @@ function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
 }
   
-function addBookToPage() {
-
+function refreshBooks() {
+  myLibrary.forEach(book => {
+    console.log(book)
+    document.querySelector(".card-container").innerHTML += `
+      <div class="card" data-id="${book.id}">
+          <div id="details">
+            <h2>${book.title}</h2>
+            <h4>${book.author}</h4>
+            <p>${book.pages}</p>
+            <p>${book.read}</p>
+          </div>
+          <div id="buttons">
+            <button>Delete?</button>
+            <button>Read?</button>
+          </div>
+      </div>
+    `;
+  });
 }
+
+refreshBooks();
 
 /* Pseudocode
 
